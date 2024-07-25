1f5e7257c412f0ac6695c9f057488ad0cb2ac873
bysunfruto <erarong22@ewha.ac.kr>
2024-07-26 02:17:02 +0900

    feat:로그인 구현

--- Backend/bingbong/accounts/serializers.py
+++ Backend/bingbong/accounts/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
-
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token 
 from rest_framework.validators import UniqueValidator
@@ -42,3 +42,18 @@ class SignupSerializer(serializers.ModelSerializer):
         user.save()
         token = Token.objects.create(user=user)
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    # write_only=True 옵션을 통해 클라이언트->서버의 역직렬화는 가능하지만, 서버->클라이언트 방향의 직렬화는 불가능하도록 해준다.
+    
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user) # 해당 유저의 토큰을 불러옴
+            return token
+        raise serializers.ValidationError( # 가입된 유저가 없을 경우
+            {"error": "일치하는 회원 정보가 없습니다."}
+        )
+    
--- Backend/bingbong/accounts/views.py
+++ Backend/bingbong/accounts/views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import SignupSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import SignupSerializer, LoginSerializer
 
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignupSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data
+        return Response({"token":token.key}, status=status.HTTP_200_OK)
