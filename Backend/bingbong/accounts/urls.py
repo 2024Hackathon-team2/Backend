f4b7906bc82be386731bef6e15dbb593a8594a94
jimin <010709min@ewhain.net>
2024-07-28 01:30:52 +0900

    fix accounts url 수정
    app_name 수정

--- Backend/bingbong/accounts/urls.py
+++ Backend/bingbong/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import SignupView, LoginView, MypageView, AddFriendView, ChangePasswordView
 
-
+app_name = 'accounts'
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
