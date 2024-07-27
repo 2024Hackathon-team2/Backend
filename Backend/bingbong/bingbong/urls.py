d6401649194aed241b9bf838b00d09441af6fe27
jimin <010709min@ewhain.net>
2024-07-28 01:23:05 +0900

    fix accounts url 수정
    accounts app_name 설정 후 url 연결

--- Backend/bingbong/bingbong/urls.py
+++ Backend/bingbong/bingbong/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+app_name = 'accounts'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
